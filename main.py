e8a7ffd8f08dfc79d28f73706dcd2087785cc9b7
belp1112 <84126001+belp1112@users.noreply.github.com>
2021-10-16 01:02:09 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import discord
+from discord import message
 from discord.ext import commands
 import os
-from functions import create_member_list, random_split_team
+from functions import create_member_list, random_split_team, wait_for_yesno, wait_for_uncount_member
 
 
 TOKEN = os.getenv('CustomGameBot')
@@ -21,45 +22,33 @@ async def ready(ctx):
         return
 
     member_list = create_member_list(ctx)
+
+
     if len(member_list) == 10:#10人の場合
         team_b = await random_split_team(ctx, member_list)
-        async def wait_for(ctx, member_list, team_b):
-            msg = await client.wait_for('message', check=check, timeout=None)
-            if msg.content == 'yes':
-                channel = ctx.guild.get_channel(844918521631866950)#かきぴ家ニートチャンネルID
-                for member in team_b:
-                    await member.move_to(channel, reason='have fun')
-                await msg.author.reply('メンバー移動しました')
-                return
-            elif msg.content == 'no':
-                await random_split_team(ctx, member_list)
-                await wait_for(ctx, member_list,team_b)
-        await wait_for(ctx, member_list, team_b)
+        await wait_for_yesno(ctx, member_list, team_b, client)
 
-    elif len(member_list) >= 3:#11人以上の場合
-        added_num_member = []
-        for i in member_list:
-            added_num_member.append[[len(range(member_list))]:[i.name]]
+
+    elif len(member_list) >= 11:#11人以上の場合
         reply_msg_over10 = '```'
-        for i in added_num_member:
-            reply_msg_over10 += f'{i}\n'
-        reply_msg_over10 += '```'
-        ctx.reply(reply_msg_over10)    
+        for i in range(len(member_list)):
+            reply_msg_over10 += f'{i+1}:{member_list[i].name}\n'
+        reply_msg_over10 += '```\n'
+        reply_msg_over10 += 'Enter the nombers of member who not join the Game with space'
+        new_member_list = await wait_for_uncount_member(ctx,member_list,reply_msg_over10,client)
+        team_b = await random_split_team(ctx,new_member_list)
+        await wait_for_yesno(ctx,new_member_list,team_b,client)
 
-    
-    elif len(member_list) <= 2:#9人以下の場合
-        await ctx.reply(f'人数が足りません。(現在{len(member_list)}人です)')
+
+    elif len(member_list) <= 9:#9人以下の場合
+        await ctx.reply(f'Members are not enough。(Currently {len(member_list)} people)')
 
 
 @client.command()
-async def id(ctx):
-    member_data = ctx.author.voice.channel.members
-    await ctx.channel.send(member_data)
-
-
-def check(m):
-    return m.content == 'yes' or m.content == 'no'
-
-
-
+async def back(ctx):
+    ovject_channel = client.get_channel(837295798114189336)
+    target_channel = client.get_channel(844918521631866950)
+    members = target_channel.members
+    for i in members:
+        pass
 client.run(TOKEN)
